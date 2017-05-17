aaf52e65393b67a530bd821cd093f1eee7517573
Stefan-Knezevic <student@stefan-knezevic.local>
2017-05-17 16:20:26 -0400

    basically finished

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -3,8 +3,9 @@ import Foundation
 class Model {
     
     var randomNumber : Int = 0
-    var operation : Operation? = nil
     var computedBool : Bool?
+    var input : String = ""
+    var b: Int = 0
     
     
     public func random(from : Int, toButNotIncluding : Int) -> Int {
@@ -50,7 +51,7 @@ class Model {
     
     func newNumber() {
         
-        randomNumber = random(from: 0, toButNotIncluding: 101)
+        randomNumber = random(from: 1, toButNotIncluding: 101)
         
     }
     
@@ -59,251 +60,86 @@ class Model {
         
         for a in stride(from: 2, to: 100, by: 1){
             if ((randomNumber % a) == 0) && (randomNumber != a){
+                b = 1
+            }
+        }
+        
+            if b == 1{
                 computedBool = false
             }
-            else{
+            else if b == 0{
                 computedBool = true
             }
+            
+        
+    }
+    
+    func divideBythree(){
+        
+        if (randomNumber % 3) == 0 {
+            
+            computedBool = true
+        }
+        else {
+            computedBool = false
+        }
+    }
+    
+    func divideByfour(){
+        
+        if (randomNumber % 4) == 0 {
+            
+            computedBool = true
+        }
+        else {
+            computedBool = false
+        }
+    }
+    
+    func divideByseven(){
+        
+        if (randomNumber % 7) == 0 {
+            
+            computedBool = true
+        }
+        else {
+            computedBool = false
+        }
+    }
+    
+    func divideBynine(){
+        
+        if (randomNumber % 9) == 0 {
+            
+            computedBool = true
+        }
+        else {
+            computedBool = false
         }
     }
     
     
-    
+    func submit(){
+        
+        if (input == "") {
+            
+            //do nothing
+            
+        }
+        else if (Int(input)!) == randomNumber{
+            
+            computedBool = true
+            
+        }
+        else if (Int(input)!) != randomNumber{
+            
+            computedBool = false
+        }
+        
+        
+    }
     
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  Calculator.swift
-//  CalculatorTestRun
-//
-//  Created by Russell Gordon on 4/27/17.
-//  Copyright © 2017 Russell Gordon. All rights reserved.
-//
-//
-//import Foundation
-//
-//class Calculator {
-//    
-//    // MARK: Properties
-//    var providedValue : String = ""
-//    var computedValue : Double? = nil
-//    var operation : Operation? = nil
-//    
-//    //type is a varible that holds the state of the plusorminus function
-//    //when it is 0 or 2 then the plusorminus function embeded in the equals function. When it equals 1 the code in the equals function runs
-//    var type : Int = 0
-//    
-//    // MARK: Initializer(s)
-//    
-//    // MARK: Method(s) a.k.a. Function(s)
-//    func addToNewValue(digit : String) {
-//        providedValue = providedValue + digit
-//    }
-//    
-//    // function that is called when the percentage button is pressed
-//    func Percent(){
-//        operation = Operation.percentage
-//        
-//        updateState()
-//        equals()
-//    }
-//    
-//    /**
-//     Sets calculator operation to multiplication, and computes a new value, if needed.
-//     */
-//    
-//    // function that is called when the multiplication button is pressed
-//    func multiply() {
-//        
-//        // Set the operation
-//        operation = Operation.multiplication
-//        
-//        updateState()
-//    }
-//    
-//    /**
-//     Sets calculator operation to division, and computes a new value, if needed.
-//     */
-//    
-//    // function that is called when the division button is pressed
-//    func divide() {
-//        
-//        // Set the operation
-//        operation = Operation.division
-//        
-//        updateState()
-//    }
-//    
-//    // function that is called when the plusorminus button is pressed
-//    func plusorminus(){
-//        if computedValue == nil && providedValue == ""{
-//            
-//            if type == 1{
-//                type = 2
-//            }
-//            else{
-//                type = 1
-//            }
-//        }
-//        else {
-//            type = 1
-//            equals()
-//        }
-//        updateState()
-//    }
-//    
-//    // function that is called when the addition button is pressed
-//    func addition(){
-//        operation = Operation.addition
-//        
-//        updateState()
-//    }
-//    
-//    // function that is called when the subtraction button is pressed
-//    func subtraction(){
-//        
-//        operation = Operation.subtraction
-//        updateState()
-//    }
-//    
-//    
-//    
-//    /**
-//     Updates calculator state.
-//     
-//     This means: what the current computed value is, what the new value provided by the user is, and whether to perform an operation on the computed value and provided value.
-//     */
-//    func updateState() {
-//        
-//        if computedValue == nil {
-//            
-//            // 1. When in this branch, there is no current computed value, just a new provided value.
-//            
-//            // Now then, the only thing to do is make the provided value become the computed value.
-//            makeProvidedValueComputedValue()
-//            
-//        } else {
-//            
-//            // 1. When in this branch, there is a current computed value.
-//            
-//            if providedValue == "" {
-//                
-//                // 2. When in this branch, there no provided value yet (an empty string: "")
-//                //    Only an operation has been logged (multiply, divide, et cetera).
-//                
-//                // So, do nothing.
-//                
-//            } else {
-//                
-//                // 2. When in this branch, a new provided value has been given.
-//                
-//                // So, perform the operation!
-//                equals()
-//            }
-//            
-//        }
-//        
-//    }
-//    
-//    /**
-//     Determine a new computed value.
-//     
-//     The current operation is performed on the computed value and the provided value.
-//     */
-//    // function that is called when the equals button is pressed
-//    func equals() {
-//        
-//        // Check operation type
-//        if operation == Operation.multiplication {
-//            computedValue = computedValue! * Double(providedValue)!
-//        } else if operation == Operation.division {
-//            computedValue = computedValue! / Double(providedValue)!
-//        }
-//        else if operation == Operation.addition{
-//            computedValue = computedValue! + Double(providedValue)!
-//        }
-//        else if operation == Operation.subtraction {
-//            computedValue = computedValue! - Double(providedValue)!
-//        }
-//        else if operation == Operation.percentage {
-//            updateState()
-//            computedValue = computedValue! / 100
-//        }
-//            // does the percentage arithmetic
-//        else if operation == Operation.percentage{
-//            
-//            if computedValue != nil{
-//                computedValue = computedValue!/100
-//            }
-//            else{
-//                computedValue = Double(providedValue)! / 100
-//            }
-//            updateState()
-//        }
-//        
-//        //used for plus or minus
-//        if type == 1{
-//            
-//            if providedValue != ""{
-//                
-//                if operation != nil {
-//                    computedValue = computedValue! * (-1)
-//                }
-//                else{
-//                    computedValue = Double(providedValue)! * (-1)
-//                }
-//            }
-//            else if computedValue != nil{
-//                computedValue = computedValue! * (-1)
-//            }
-//            type = 2
-//        }
-//        
-//        // The operation selected has been performed, so get ready to receive new operation
-//        // and new value
-//        operation = nil
-//        providedValue = ""
-//    }
-//    
-//    /**
-//     Makes the computed value become whatever value the user has typed into the calculator.
-//     */
-//    func makeProvidedValueComputedValue() {
-//        
-//        computedValue = Double(providedValue)
-//        providedValue = ""
-//    }
-//    
-//    /**
-//     Resets the operation, provided value, and computed value.
-//     */
-//    // function that is called when the clear button is pressed
-//    func clear() {
-//        
-//        // Reset everthing
-//        type = 0
-//        operation = nil
-//        providedValue = ""
-//        computedValue = nil
-//    }
-//    
-//}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -21,10 +21,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var dividedByseven: UIButton!
     @IBOutlet weak var dividedBynine: UIButton!
     @IBOutlet weak var dividedBy: UIButton!
+    @IBOutlet weak var Textfield: UITextField!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelDisplay.text = ""
+        labelDisplay.text = "Try and guess my number!"
         model.newNumber()
         
         dividedBythree.isHidden = true
@@ -53,53 +57,89 @@ class ViewController: UIViewController {
             dividedBynine.isHidden = false
             
         }
-        
-        
-        
     }
     
     
     @IBAction func dividedBythreePressed(_ sender: Any) {
         
-        
+        model.divideBythree()
         
         dividedBy.isHidden = false
         dividedBythree.isHidden = true
         dividedByfour.isHidden = true
         dividedByseven.isHidden = true
         dividedBynine.isHidden = true
+        
+        if model.computedBool == true {
+            
+            labelDisplay.text = String("True")
+        }
+        else if model.computedBool == false {
+            
+            labelDisplay.text = String("False")
+        }
     }
     
     @IBAction func dividedByfourPressed(_ sender: Any) {
         
+        model.divideByfour()
+        
         dividedBy.isHidden = false
         dividedBythree.isHidden = true
         dividedByfour.isHidden = true
         dividedByseven.isHidden = true
         dividedBynine.isHidden = true
+        
+        if model.computedBool == true {
+            
+            labelDisplay.text = String("True")
+        }
+        else if model.computedBool == false {
+            
+            labelDisplay.text = String("False")
+        }
     }
     
     @IBAction func dividedBysevenPressed(_ sender: Any) {
         
-        
+        model.divideByseven()
         
         dividedBy.isHidden = false
         dividedBythree.isHidden = true
         dividedByfour.isHidden = true
         dividedByseven.isHidden = true
         dividedBynine.isHidden = true
+        
+        if model.computedBool == true {
+            
+            labelDisplay.text = String("True")
+        }
+        else if model.computedBool == false {
+            
+            labelDisplay.text = String("False")
+        }
     }
     
     
     @IBAction func dividedByninePressed(_ sender: Any) {
         
-        
+        model.divideBynine()
         
         dividedBy.isHidden = false
         dividedBythree.isHidden = true
         dividedByfour.isHidden = true
         dividedByseven.isHidden = true
         dividedBynine.isHidden = true
+        
+        if model.computedBool == true {
+            
+            labelDisplay.text = String("True")
+        }
+        else if model.computedBool == false {
+            
+            labelDisplay.text = String("False")
+        }
+        
     }
     
     
@@ -170,195 +210,41 @@ class ViewController: UIViewController {
             
             labelDisplay.text = String("False")
         }
+        
+        
     }
     
     
     
     
     @IBAction func submitPressed(_ sender: Any) {
+ 
+        if Textfield.text != nil{
         
+            model.input = Textfield.text!
+        }
+        
+        model.submit()
+        
+        if model.input == ""{
+            labelDisplay.text = String("Hey! That's not a number!")
+            
+        }
+        else if model.computedBool == true {
+            
+            labelDisplay.text = String("Congrats! You guessed my number!")
+        }
+        else if model.computedBool == false {
+            
+            labelDisplay.text = String("Sorry, you're Wrong!")
+        }
+        
+        model.newNumber()
+        
+        Textfield.text = ""
         
     }
-    
-    
-    
-    
-    
-    
 }
-
-
-
-
-
-
-//class ViewController: UIViewController {
-//
-//    //connection to view
-//
-//    @IBOutlet weak var labelDisplay: UILabel!
-//
-//    //Create instance of Calculator class
-//
-//    var model = Calculator()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//        //clear the contents of the label
-//        labelDisplay.text = ""
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-//    //The actions which provide value to the buttons
-//    //When one is pressed
-//    @IBAction func onePressd(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "1")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When two is pressed
-//    @IBAction func twoPressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "2")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When three is pressed
-//    @IBAction func threePressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "3")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When four is pressed
-//    @IBAction func fourPressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "4")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When five is pressed
-//    @IBAction func fivePressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "5")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When six is pressed
-//    @IBAction func sixPressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "6")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When seven is pressed
-//    @IBAction func sevenPressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "7")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When eight is pressed
-//    @IBAction func eightPressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "8")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When nine is pressed
-//    @IBAction func ninePressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "9")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When zero is pressed
-//    @IBAction func zeroPressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: "0")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //When the period is pressed
-//    @IBAction func periodPressed(_ sender: Any) {
-//
-//        model.addToNewValue(digit: ".")
-//        labelDisplay.text = model.providedValue
-//    }
-//
-//    //arithmetic operator actions
-//
-//    //When multiplication is pressed
-//    @IBAction func multiply(_ sender: Any) {
-//
-//        model.multiply()
-//         labelDisplay.text = String("✕")
-//    }
-//
-//    //When division is pressed
-//    @IBAction func divide(_ sender: Any) {
-//
-//        model.divide()
-//         labelDisplay.text = String("÷")
-//    }
-//
-//    //When addition is pressed
-//    @IBAction func addition(_ sender: Any) {
-//
-//        model.addition()
-//         labelDisplay.text = String("+")
-//    }
-//
-//    //When subtraction is pressed
-//    @IBAction func subtraction(_ sender: Any) {
-//
-//        model.subtraction()
-//        labelDisplay.text = String("-")
-//    }
-//
-//    //function actions (such as clear, equal, plus or minus etc.)
-//
-//    //When equals is pressed
-//    @IBAction func equals(_ sender: Any) {
-//        model.equals()
-//
-//        if (model.computedValue != nil){
-//            labelDisplay.text = String(format: "%g", model.computedValue!)
-//        }
-//    }
-//
-//    //When clear is pressed
-//    @IBAction func clear(_ sender: Any) {
-//
-//        model.clear()
-//        labelDisplay.text = String("0")
-//    }
-//
-//    //When plusorminus is pressed
-//    @IBAction func plusorminus(_ sender: Any) {
-//        model.plusorminus()
-//
-//        if (model.computedValue != nil){
-//            labelDisplay.text = String(format: "%g", model.computedValue!)
-//        }
-//    }
-//
-//    //When percent is pressed
-//    @IBAction func Percent(_ sender: Any) {
-//
-//        model.Percent()
-//        if (model.computedValue != nil){
-//            labelDisplay.text = String(format: "%g", model.computedValue!)
-//        }
-//    }
-//}
-
 
 
 
